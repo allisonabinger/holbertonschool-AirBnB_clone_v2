4d3c244e682f90e7796f15cb128a66edd6548667
Allison <allisonabinger@gmail.com>
2023-11-13 16:35:10 -0600

    template is complete, working on .py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -48,7 +48,8 @@ def show_num(n):
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def num_temp(n)
+def num_temp(n):
+    return render_remplate('5-number.html', n=n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
