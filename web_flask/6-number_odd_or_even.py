3b525be065d783f233792397cb3be6de88c5abc4
Allison <allisonabinger@gmail.com>
2023-11-13 16:45:30 -0600

    6 is fully functional!

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -63,7 +63,7 @@ def num_even_odd(n):
         result = "even"
     else:
         result = "odd"
-    return render_template('6-number_odd_or_even.html', n=n, result=r)
+    return render_template('6-number_odd_or_even.html', n=n, result=result)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
