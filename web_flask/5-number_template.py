db7b57a16104dacd23d4cc08526799cc2845b615
Allison <allisonabinger@gmail.com>
2023-11-13 16:36:15 -0600

    errors in py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -49,7 +49,7 @@ def show_num(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def num_temp(n):
-    return render_remplate('5-number.html', n=n)
+    return render_template('5-number.html', n=n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
