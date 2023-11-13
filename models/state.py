a6549a5235592a9f4209ebaedd6f199ae8e52967
Allison <allisonabinger@gmail.com>
2023-11-13 17:11:58 -0600

    added getter to states

--- models/state.py
+++ models/state.py
@@ -7,14 +7,23 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class State(BaseModel, Base):
     """Representation of state """
-    if models.storage_t == "db":
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
-    else:
-        name = ""
-    cites = []
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    cities = relationship("City", backref="state", cascade="delete")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """getter method to return list (city_list) of 
+            City objects from storage linked to current State"""
+            city_list = []
+            for city in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
