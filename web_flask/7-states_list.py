73e446fa35201153159fa60ddb95901f7bec387c
Allison <allisonabinger@gmail.com>
2023-11-14 10:44:49 -0600

    pep8 - still unable to test in local machine but passing checks

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """returns the key/value pair for a list of states"""
-    states = storage.all(State).values() 
+    states = storage.all(State).values()
     """fetching states from storage - gettings list of dictionary objects"""
     return render_template('7-states_list.html', states=states)
 
